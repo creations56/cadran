08be9109d3d1c005018a9bafe676889c3611a95b
creations56 <74179508+creations56@users.noreply.github.com>
2022-10-07 17:29:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 // definition des constantes / variables
 
-let now = new Date(); // la date et heure actuelle
+
 let latitude = 0;
 let longitude = 0;
-let localisation = document.getElementById('localisation');
 let jour=1;
 let mois=1;
 let annee=2020;
+
+let localisation = document.getElementById('localisation');
 let entree = document.getElementById("in");
 let date2 = document.getElementById("date2");
 let latitude_in=document.getElementById("latitude");
 let longitude_in=document.getElementById("longitude");
 
+let now = new Date(); // la date et heure actuelle
+
+// permet de declencher une action si click sur bouton "bouton1"
+document.getElementById('bouton1').addEventListener('click', boutonAppuye);
+
+
 // date et heure
 jour=now.getDate();
 mois=now.getMonth()+1;
 annee=now.getFullYear();
-date1.textContent = "Nous sommes le : "+jour+"/"+mois+"/"+annee;
+date1.textContent = now;
 date2.value = jour+"/"+mois+"/"+annee;
 
 function getValue() {
@@ -27,15 +34,13 @@ function getValue() {
     entree.value=entree_valeur+" modifié";   
   }
 
-// permet de declencher une action si clicksur bouton "bouton1"
-document.getElementById('bouton1').addEventListener('click', boutonAppuye);
-
+  
 function boutonAppuye() {
   localisation.textContent="bouton appuyé";
-  getLocation();
-  localisation.textContent= "Latitude ="+latitude+" , Longitude ="+longitude;  
-  latitude_in.value=latitude;
-  longitude_in.value=longitude;
+  getLocation2();
+  //localisation.textContent= "Latitude ="+latitude+" , Longitude ="+longitude;  
+  //latitude_in.value=latitude;
+  //longitude_in.value=longitude;
 }
 
 function getLocation() {
@@ -51,6 +56,23 @@ function getLocation() {
     longitude= 0;
     localisation.textContent  = "Geolocation is not supported by this browser.";
   }
+}
+
+// alternative geolocalisation
+
+
+
+function getLocation2() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    localisation.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  localisation.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
 }
 
 
