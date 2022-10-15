d774d4491a5773b92fe2da08667f14ef44a66ab3
creations56 <74179508+creations56@users.noreply.github.com>
2022-10-15 13:39:37 +0200

    Add files via upload

--- calend.js
+++ calend.js
@@ -1,20 +1,38 @@
-var jour=1;
+var HL=new Date();
+var HSM= new Date();
+var HSV=new Date();
+var jour=1;// heure systeme
 var mois=1;
 var annee=1;
 var heure=1;
 var minute=1;
 var seconde=1;
-var jourUTC=1;
+var jourUTC=1;// ? UTC
 var moisUTC=1;
 var anneeUTC=1;
 var heureUTC=1;
 var minuteUTC=1;
 var secondeUTC=1;
-var timeZone=0;
+var timeZone=0; //
+var ecartUTCLong=0; // 
+var heureHL=0; // heure legale
+var minuteHL=0;
+var heureUTC=0; // heure UTC
+var minuteUTC=0;
+var heureHSM=0; // heure solaire moyenne
+var minuteHSM=0;
+var heureHSV=0; // heure solaire vraie
+var minuteHSV=0;
+var decalageHL=0; // decalage heures par rapport a HL
+var decalageHSM=0;
+var decalageHSV=0;
 const nomMois = ['jan', 'fev', 'mars', 'avr', 'mai', 'juin', 'juil', 'aout', 'sept', 'oct','nov','dec'];
 var lat=999;  // latitude
 var long=999; // longitude
 // ne pas utiliser de variable pour les elements definis par id
+
+
+// ---------- calcul heure systeme et heure UTC----------
 
 function refresh(){
 var t = 1000; // rafraîchissement en millisecondes
@@ -34,7 +52,8 @@ function ajouteZero(val){
 }
 
 function majHeure()  {
-let now = new Date(); // la date et heure actuelle
+var now = new Date(); // la date et heure actuelle 
+//HL.setTime(now.getTime());
 let formatHeure="";
 let formatDate="";
 jour=now.getDate();
@@ -49,46 +68,88 @@ anneeUTC=now.getUTCFullYear();
 heureUTC=now.getUTCHours();
 minuteUTC=now.getUTCMinutes();
 secondeUTC=now.getUTCSeconds();
-
 timeZone=String(-1*now.getTimezoneOffset()/60);
 if (now.getTimezoneOffset()<0){
   timeZone="+"+timeZone;
-}
-
+  }
 // affiche heure systeme
 formatHeure=ajouteZero(heure)+" H "+ajouteZero(minute)+" mn "+ajouteZero(seconde)+" s";
 formatDate=ajouteZero(jour)+" "+nomMois[mois]+" "+annee;
 document.getElementById("l1c1").textContent = formatDate+"\n"+formatHeure+"\n"+"fuseau horaire GMT "+timeZone;
-
 // affiche heure UTC
-formatHeure=ajouteZero(heureUTC)+" H "+ajouteZero(minuteUTC)+" mn "+ajouteZero(secondeUTC)+" s";
-formatDate=ajouteZero(jourUTC)+" "+nomMois[moisUTC]+" "+anneeUTC;
-document.getElementById("l1c2").textContent = formatDate+"\n"+formatHeure;
+//formatHeure=ajouteZero(heureUTC)+" H "+ajouteZero(minuteUTC)+" mn "+ajouteZero(secondeUTC)+" s";
+//formatDate=ajouteZero(jourUTC)+" "+nomMois[moisUTC]+" "+anneeUTC;
+//document.getElementById("l1c2").textContent = formatDate+"\n"+formatHeure;
 refresh();
 }
 
-function initialisation(){
-  majHeure();
+// -------- affiche heure legale -------
+function afficheHL(){
+  let formatHeure="";
+  heureHL=HL.getHours();
+  minuteHL=HL.getMinutes();
+  decalageHL=String(-1*HL.getTimezoneOffset()/60);
+  if (HL.getTimezoneOffset()<0){decalageHL="+"+decalageHL;}
+  formatHeure=ajouteZero(heureHL)+" H "+ajouteZero(minuteHL)+" mn"+"\n"+"GMT "+decalageHL;
+  document.getElementById("l3").value=formatHeure;
+}
+
+// -------- affiche heure UTC -------
+function afficheUTC(){
+  let formatHeure="";
+  heureUTC=HL.getUTCHours();
+  minuteUTC=HL.getUTCMinutes();
+  formatHeure=ajouteZero(heureUTC)+" H "+ajouteZero(minuteUTC)+" mn ";
+  document.getElementById("l2").value=formatHeure;
+}
+
+// ----- calcul heure solaire moyenne , heure solaire vraie----------
+
+function calculHSM() {
+  ecartHeureLong=long/15-decalageHL;// ecart en heures lie a la longitude
+  decalageHSM=long/15-decalageHL;
+  //ecartHeureLong=0;
+  // setTime getTime donne l'heure en millisecondes
+  HSM.setTime(HL.getTime() + (ecartHeureLong*60*60*1000));
+  let formatHeure=""; // variable locale
+  heureHSM=HSM.getHours();
+  minuteHSM=HSM.getMinutes();
+  formatHeure=ajouteZero(heureHSM)+" H "+ajouteZero(minuteHSM)+" mn ";
+  document.getElementById("l4").value=formatHeure;
+}
+
+function calculHSV() {
+  let nombreJours=0;
+  let B=0;
+  let deltaT=0; // delta T en minutes
+  let premierJanvier= new Date(HL.getFullYear(),0,1,0);// premier janvier
+  //premierJanvier.setUTCHours(-2);
+  //let temp= new Date(HL.getFullYear(),2,20,0);//  
+  let diffTemps = HL.getTime() - premierJanvier.getTime(); 
+  nombreJours =  Math.floor(diffTemps / (1000 * 3600 * 24)+1); 
+  B=2*Math.PI*(nombreJours-81)/365; 
+  deltaT=7,678*Math.sin(B+1,374)-9,87*Math.sin(2*B);
+  //alert("Le nombre de jours entre les deux dates est de " + diffJours);
+  //alert(premierJanvier)
+  
+  decalageHSV=long/15-decalageHL+deltaT/60;//
+  HSV.setTime(HL.getTime() + (decalageHSV*60*60*1000));
+  let formatHeure=""; // variable locale
+  heureHSV=HSV.getHours();
+  minuteHSV=HSV.getMinutes();
+  formatHeure=ajouteZero(heureHSV)+" H "+ajouteZero(minuteHSV)+" mn ";
+  document.getElementById("l5").value=formatHeure;
 }
 
 // ---------- geolocalisation ----------
 
-
-
-function affichage(){
-  document.getElementById("l3c1").value="Latitude: " + lat + "\n"+"longitude: " + long;
-  //let logArea = document.getElementById("l3c1");
-  //logArea.value = "";
-  //logArea.value += "Latitude: " + position.coords.latitude + "\n";
-  //logArea.value += "Longitude: " + position.coords.longitude + "\n";
+function affichageLatLong(){
+  document.getElementById("l3c1").value="latitude: " + lat + "°\n"+"longitude: " + long+"°";
+  if (long!=999) {calculHSM();calculHSV()}// si valeur long entrée afficher HSM et HSV
 }
 
 function successHandler(position)  {
   // Success Handler
-  //let logArea = document.getElementById("l3c1");
-  //logArea.value = "";
-  //logArea.value += "Latitude: " + position.coords.latitude + "\n";
-  //logArea.value += "Longitude: " + position.coords.longitude + "\n";
   lat=position.coords.latitude;
   long=position.coords.longitude;
 }
@@ -107,13 +168,29 @@ function errorHandler(positionError)  {
 function miseEnAttente(){
   // appel d'affichage
   document.getElementById("l3c1").value="attente des valeurs de geolocalisation (environ 2 secondes) ...";
-    setTimeout(affichage, 2000); //On attend 5 secondes avant d'exécuter la fonction
+    setTimeout(affichageLatLong, 2000); //On attend 5 secondes avant d'exécuter la fonction
 }
 
 function affichageCoord()  {
   document.getElementById("l3c1").value="attente des valeurs de geolocalisation (environ 2 secondes) ...";
   navigator.geolocation.getCurrentPosition(successHandler, errorHandler);
   miseEnAttente();// if faut attendre que successHandler retourne les valeurs de lat et long
+  }
+  
+  function validCoordManuel() {
+    lat = document.getElementById("latM").value;
+    long= document.getElementById("longM").value;
+    if (lat>90) {lat=90;}
+    if (lat<-90) {lat=-90;}
+    if (long>180) {long=180;}
+    if (long<-180) {long=-180;}
+    affichageLatLong();
+  }
+ 
+// ---------- geolocalisation ---------- 
+  function initialisation(){
+  majHeure();
+  HL = new Date();
+  afficheHL();
+  afficheUTC();
 }
-
-
